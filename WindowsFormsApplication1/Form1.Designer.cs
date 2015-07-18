01c8b6e970ff26c10e932f412ef063e88788b0ed
n-matsu <narimichi.matsumoto@gmail.com>
2015-07-18 17:21:47 +0900

    draw type

--- WindowsFormsApplication1/Form1.Designer.cs
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -28,21 +28,79 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.rdoEllipse = new System.Windows.Forms.RadioButton();
+            this.rdoRect = new System.Windows.Forms.RadioButton();
+            this.rdoLine = new System.Windows.Forms.RadioButton();
+            this.panel1.SuspendLayout();
             this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.rdoEllipse);
+            this.panel1.Controls.Add(this.rdoRect);
+            this.panel1.Controls.Add(this.rdoLine);
+            this.panel1.Location = new System.Drawing.Point(12, 12);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(267, 24);
+            this.panel1.TabIndex = 0;
+            // 
+            // rdoEllipse
+            // 
+            this.rdoEllipse.AutoSize = true;
+            this.rdoEllipse.Location = new System.Drawing.Point(196, 5);
+            this.rdoEllipse.Name = "rdoEllipse";
+            this.rdoEllipse.Size = new System.Drawing.Size(57, 16);
+            this.rdoEllipse.TabIndex = 0;
+            this.rdoEllipse.TabStop = true;
+            this.rdoEllipse.Text = "Ellipse";
+            this.rdoEllipse.UseVisualStyleBackColor = true;
+            // 
+            // rdoRect
+            // 
+            this.rdoRect.AutoSize = true;
+            this.rdoRect.Location = new System.Drawing.Point(94, 5);
+            this.rdoRect.Name = "rdoRect";
+            this.rdoRect.Size = new System.Drawing.Size(74, 16);
+            this.rdoRect.TabIndex = 0;
+            this.rdoRect.TabStop = true;
+            this.rdoRect.Text = "Rectangle";
+            this.rdoRect.UseVisualStyleBackColor = true;
+            // 
+            // rdoLine
+            // 
+            this.rdoLine.AutoSize = true;
+            this.rdoLine.Checked = true;
+            this.rdoLine.Location = new System.Drawing.Point(15, 5);
+            this.rdoLine.Name = "rdoLine";
+            this.rdoLine.Size = new System.Drawing.Size(44, 16);
+            this.rdoLine.TabIndex = 0;
+            this.rdoLine.TabStop = true;
+            this.rdoLine.Text = "Line";
+            this.rdoLine.UseVisualStyleBackColor = true;
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(673, 438);
+            this.ClientSize = new System.Drawing.Size(673, 404);
+            this.Controls.Add(this.panel1);
             this.Name = "Form1";
             this.Text = "Form1";
             this.Load += new System.EventHandler(this.Form1_Load);
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
             this.ResumeLayout(false);
 
         }
 
         #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.RadioButton rdoEllipse;
+        private System.Windows.Forms.RadioButton rdoRect;
+        private System.Windows.Forms.RadioButton rdoLine;
+
     }
 }
 
--- WindowsFormsApplication1/Form1.cs
+++ WindowsFormsApplication1/Form1.cs
@@ -3,6 +3,7 @@ using System.Data;
 using System.Drawing;
 using System.Linq;
 using System.Windows.Forms;
+using System.Reactive;
 using System.Reactive.Linq;
 
 namespace WindowsFormsApplication1
@@ -38,8 +39,18 @@ namespace WindowsFormsApplication1
             .Select(evt => evt.Location)
             .Buffer(2, 1)
             .Repeat()
-            .Where(points => points.Count > 1)
+            .Where(points => rdoLine.Checked && points.Count > 1)
             .Subscribe(points => this.DrawLine(points[0], points[1]));
+
+            mousedown
+            .Zip(mouseup, (evt, evt2) => new { pt1 = evt.Location, pt2 = evt2.Location })
+            .Where(_ => rdoRect.Checked)
+            .Subscribe(points => this.DrawRect(points.pt1, points.pt2));
+
+            mousedown
+            .Zip(mouseup, (evt, evt2) => new { pt1 = evt.Location, pt2 = evt2.Location })
+            .Where(_ => rdoEllipse.Checked)
+            .Subscribe(points => this.DrawElipse(points.pt1, points.pt2));
         }
 
         private void DrawLine(Point point1, Point point2)
@@ -47,6 +58,30 @@ namespace WindowsFormsApplication1
             using (var g = this.CreateGraphics())
             {
                 g.DrawLine(Pens.ForestGreen, point1, point2);
+            }
+        }
+
+        private void DrawRect(Point point1, Point point2)
+        {
+            using (var g = this.CreateGraphics())
+            {
+                g.DrawRectangle(Pens.MidnightBlue,
+                    Math.Min(point1.X, point2.X),
+                    Math.Min(point1.Y, point2.Y),
+                    Math.Abs(point1.X - point2.X),
+                    Math.Abs(point1.Y - point2.Y));
+            }
+        }
+
+        private void DrawElipse(Point point1, Point point2)
+        {
+            using (var g = this.CreateGraphics())
+            {
+                g.DrawEllipse(Pens.MidnightBlue,
+                    Math.Min(point1.X, point2.X),
+                    Math.Min(point1.Y, point2.Y),
+                    Math.Abs(point1.X - point2.X),
+                    Math.Abs(point1.Y - point2.Y));
             }
         }
     }
